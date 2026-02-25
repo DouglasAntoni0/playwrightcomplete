b1655b3a9fa339b352da2f842435c03acddb77c4
Douglas Antonio <douglas.doug1235@gmail.com>
2026-02-25 19:55:23 -0300

    Melhorias em aplicações

--- projects/zombieplus/tests/e2e/leads.spec.js
+++ projects/zombieplus/tests/e2e/leads.spec.js
@@ -1,67 +1,48 @@
-
 import { test, expect } from '@playwright/test';
-import { LandingPage } from './pages/LandingPage';
+import { LandingPage } from '../pages/LandingPage';
 
-let landingPage
+let landingPage;
 
-test.beforeEach(async ({page}) => {
-  landingPage = new LandingPage(page)
-})
-
+test.beforeEach(async ({ page }) => {
+  landingPage = new LandingPage(page);
+});
 
 test('deve cadastrar um lead na fila de espera', async ({ page }) => {
-
   await landingPage.visit();
-
   await landingPage.openLeadModal();
-
   await landingPage.submitLeadForm('Douglas Antonio', 'douglas@zombieplus.com');
 
   const message = 'Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!';
-
   await landingPage.toastHaveText(message);
 });
 
 test('não deve cadastrar com email incorreto', async ({ page }) => {
-
   await landingPage.visit();
-
   await landingPage.openLeadModal();
-
   await landingPage.submitLeadForm('Douglas Antonio', 'dougla.com');
 
-  await landingPage.alertHaveText('Email incorreto')
+  await landingPage.alertHaveText('Email incorreto');
 });
 
 test('não deve cadastrar quando o nome não é preenchido', async ({ page }) => {
-
   await landingPage.visit();
-
   await landingPage.openLeadModal();
-
   await landingPage.submitLeadForm('', 'douglas@zombieplus.com');
 
   await landingPage.alertHaveText('Campo obrigatório');
 });
 
 test('não deve cadastrar quando o email não é preenchido', async ({ page }) => {
-
   await landingPage.visit();
-
   await landingPage.openLeadModal();
-
   await landingPage.submitLeadForm('Douglas Antonio', '');
 
   await landingPage.alertHaveText('Campo obrigatório');
 });
 
 test('não deve cadastrar quando nenhum campo é preenchido', async ({ page }) => {
-  const landingPage = new LandingPage(page);
-
   await landingPage.visit();
-
   await landingPage.openLeadModal();
-
   await landingPage.submitLeadForm('', '');
 
   await landingPage.alertHaveText([
@@ -69,5 +50,3 @@ test('não deve cadastrar quando nenhum campo é preenchido', async ({ page }) =
     'Campo obrigatório'
   ]);
 });
-
-
