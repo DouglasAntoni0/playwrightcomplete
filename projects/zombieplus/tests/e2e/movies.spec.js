24276bc60f3664ab9ac17a5528ddc993faeffa58
Douglas Antonio <douglas.doug1235@gmail.com>
2026-03-22 14:51:10 -0300

    Atualizações

--- projects/zombieplus/tests/e2e/movies.spec.js
+++ projects/zombieplus/tests/e2e/movies.spec.js
@@ -1,6 +1,6 @@
 import { expect, test } from '../support';
 import data from '../support/fixtures/movies.json';
-import { executeSQL } from '../support/database'; 
+import { executeSQL } from '../support/database';
 
 test('deve cadastrar um novo filme', async ({ page }) => {
   const movie = data.create;
@@ -18,8 +18,20 @@ test('deve cadastrar um novo filme', async ({ page }) => {
     movie.featured
   );
 
-  await page.toast.haveText('UhullCadastro realizado com sucesso!');
+  await page.popup.haveText(`O filme '${movie.title}' foi adicionado ao catálogo.`);
 });
+
+test('deve poder remover um filme', async ({ page, request }) => {
+  const movie = data.to_remove
+  await executeSQL(`DELETE FROM movies WHERE title = '${movie.title}';`);
+  await request.api.postMovie(movie)
+  await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin');
+
+  await page.movies.remove(movie.title);
+
+  await page.popup.haveText('Filme removido com sucesso.')
+
+})
 
 test('não deve cadastrar quando o titulo já existe', async ({ page, request }) => {
   const movie = data.duplicate;
@@ -28,7 +40,7 @@ test('não deve cadastrar quando o titulo já existe', async ({ page, request })
   await request.api.postMovie(movie);
 
   await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin');
-  
+
   await page.movies.create(
     movie.title,
     movie.overview,
