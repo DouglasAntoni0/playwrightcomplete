621e6a505047ff01ce4760f8f211694bdb56150f
Douglas Antonio <douglas.doug1235@gmail.com>
2026-06-07 14:47:24 -0300

    fix(ci): run tests only on Chromium to reduce CI resource usage

--- projects/zombieplus/playwright.config.js
+++ projects/zombieplus/playwright.config.js
@@ -23,29 +23,5 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-    {
-      name: 'Mobile Chrome',
-      use: { ...devices['Pixel 5'] },
-    },
-    {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
-    },
-    {
-      name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    },
-    {
-      name: 'Google Chrome',
-      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    },
   ],
 });
